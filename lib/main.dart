4e2b255369412f334339bdee2ca2772a3270e2a2
Touza Isaac <62522441+Touza-Isaac@users.noreply.github.com>
2024-06-09 23:33:09 +0100

    update main.dart
    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_app/jsonMain.dart';
 //import 'package:todo_list_app/jsonMain.dart';
 import 'package:todo_list_app/listMain.dart';
 
 void main() {
-  runApp(const TodoListApp());
+  runApp(const TodoListAppJson());
 }
